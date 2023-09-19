fd7c9078e001b024590d4a751dd8ee915027d152
EduardViarheichyk <EduardViarheichyk@gmail.com>
2023-09-19 18:15:53 +0300

    lesson 24 1st commit

---
+++ lesson24/src/main/java/org/lesson24/DeleteServlet.java
@@ -0,0 +1,26 @@
+package org.lesson24;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/delete")
+public class DeleteServlet extends HttpServlet {
+
+    private final DataBase dataBase = new DataBase();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String deleteId = req.getParameter("deleteId");
+        if (deleteId != null && !deleteId.isBlank()) {
+            UUID uuid = UUID.fromString(deleteId);
+            dataBase.deleteCar(uuid);
+        }
+        req.getRequestDispatcher("/home").forward(req, resp);
+
+    }
+}
---
+++ lesson24/src/main/java/org/lesson24/MainServlet.java
@@ -0,0 +1,31 @@
+package org.lesson24;
+
+import org.lesson24.domain.Car;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/home")
+public class MainServlet extends HttpServlet {
+
+    private final DataBase dataBase = new DataBase();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<Car> cars = dataBase.getByBrand(null);
+        req.setAttribute("cars", cars);
+
+        req.getRequestDispatcher("home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
